63cbaadc8136fb01a3a8cd693b35f0ab7e6d6ad7
unknown <ravikumarsheokand1985@gmail.com>
2025-12-20 20:59:29 +0530

    update

--- modules/8.azurerm_sql_database/main.tf
+++ modules/8.azurerm_sql_database/main.tf
@@ -2,6 +2,6 @@ resource "azurerm_mysql_flexible_database" "sql_database" {
   name                = var.sql_database_name
   resource_group_name = var.rg_name
   server_name         = var.sql_server_name
-  charset             = "utf8"
-  collation           = "utf8_unicode_ci"
+  charset             = "utf8mb3"
+  collation           = "utf8mb3_unicode_ci"
 }
--- modules/9.azurerm_key_vault/main.tf
+++ modules/9.azurerm_key_vault/main.tf
@@ -11,8 +11,6 @@ resource "azurerm_key_vault" "kv" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    secret_permissions = ["Get", "Set", "List"]
+    secret_permissions = ["Get", "Set", "List", "Delete"]
   }
 }
-
-
