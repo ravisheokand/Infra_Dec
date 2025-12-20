b8fd54591699cd546b17f62dde442856af524ca6
unknown <ravikumarsheokand1985@gmail.com>
2025-12-20 15:22:26 +0530

    update

--- infra/dev/provider.tf
+++ infra/dev/provider.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-#      version = "~>4.43.0"
+      version = "~>4.56.0"
     }
   }
   backend "azurerm" {
@@ -16,4 +16,4 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "ff1980ab-1170-4014-971d-df65aabf1e5b"
-}  
+}
